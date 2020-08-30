cc17b666d317de1c213da43e52ba53e83c7869d1
Tom's Personal <thomasbwsu@gmail.com>
2020-08-30 17:08:46 -0400

    Finished App v1
    Working UI

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const income = Math.abs(transactions.reduce((acc,transaction) => transaction.amount > 0 ? acc + transaction.amount : acc,0).toFixed(2));
+  const expenses = Math.abs(transactions.reduce((acc,transaction) => transaction.amount < 0 ? acc + transaction.amount : acc,0).toFixed(2));
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">+${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">-${expenses}</p>
       </div>
     </div>
   )
